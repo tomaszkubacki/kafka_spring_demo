250fc5a75b8989e6d87816ba5a31a8fb4221f3cf
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2026-09-14 22:10:11 +0200

    feat: open module

---
+++ src/main/java/org/kafkaspring/demo/customer/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package org.kafkaspring.demo.customer;
+
+import org.springframework.modulith.ApplicationModule;
